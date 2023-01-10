6705b86f32dea33fe92601cf35b3f9e95e9cbe5b
Tim Kalaiko <a.kalayko@gmail.com>
2023-01-10 10:21:15 +0200

    initial commit

---
+++ BeerBuddy/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  BeerBuddy
+//
+//  Created by Tim on 10.01.2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+        return true
+    }
+}
+
---
+++ BeerBuddy/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  BeerBuddy
+//
+//  Created by Tim on 10.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+        
+    }
+}
+

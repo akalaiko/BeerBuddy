f77886534a6c260b2fcd70e9d6f32477e0180de6
Polina Tikhomirova <t_polinas@icloud.com>
2023-01-12 18:30:28 +0300

    Added custom colors.

---
+++ BeerBuddy/Core/Extensions/UIColor+Extensions.swift
@@ -0,0 +1,45 @@
+//
+//  UIColor+Extensions.swift
+//  BeerBuddy
+//
+//  Created by Polina Tikhomirova on 12.01.2023.
+//
+
+import UIKit
+
+extension UIColor {
+    static struct AppColors {
+        static let sand = UIColor(
+            red: 0.87,
+            green: 0.67,
+            blue: 0.55,
+            alpha: 1.00)
+        static let swamp = UIColor(
+            red: 0.24,
+            green: 0.27,
+            blue: 0.20,
+            alpha: 1.00)
+        static let light = UIColor(
+            red: 0.95,
+            green: 0.92,
+            blue: 0.88,
+            alpha: 1.00)
+        static let brown = UIColor(
+            red: 0.45,
+            green: 0.31,
+            blue: 0.20,
+            alpha: 1.00)
+        static let black = UIColor(
+            red: 0.00,
+            green: 0.00,
+            blue: 0.00,
+            alpha: 1.00)
+        static let offwhite = UIColor(
+            red: 1.00,
+            green: 1.00,
+            blue: 1.00,
+            alpha: 1.00)
+    }
+}
+
+

7b48f56d86fd570fe6e4472b118405cc8ca1db80
Polina Tikhomirova <t_polinas@icloud.com>
2023-01-12 20:55:48 +0300

    Updated color's extensions.

--- BeerBuddy/Core/Extensions/UIColor+Extensions.swift
+++ BeerBuddy/Core/Extensions/UIColor+Extensions.swift
@@ -7,26 +7,11 @@
 
 import UIKit
 
-struct AppColors {
-    static let sand = UIColor(hex: 0xDEAC8B)
-    static let swamp = UIColor(hex: 0x3D4632)
-    static let light = UIColor(hex: 0xF3EBE0)
-    static let brown = UIColor(hex: 0x734E33)
-    static let black = UIColor(hex: 0x000000)
-    static let offwhite = UIColor(hex: 0xFFFFFF)
-}
-
 extension UIColor {
-    convenience init(
-        hex: UInt,
-        alpha: CGFloat = 1
-    ) {
-        self.init(
-            red: CGFloat((hex & 0xFF0000) >> 16) / 255.0,
-            green: CGFloat((hex & 0x00FF00) >> 8) / 255.0,
-            blue: CGFloat(hex & 0x0000FF) / 255.0,
-            alpha: alpha)
+    convenience init(hex: UInt, alpha: CGFloat = 1) {
+        self.init( red: CGFloat((hex & 0xFF0000) >> 16) / 255.0,
+                   green: CGFloat((hex & 0x00FF00) >> 8) / 255.0,
+                   blue: CGFloat(hex & 0x0000FF) / 255.0,
+                   alpha: alpha)
     }
 }
-
-
---
+++ BeerBuddy/Core/Namespace/AppStyles.swift
@@ -0,0 +1,19 @@
+//
+//  AppStyles.swift
+//  BeerBuddy
+//
+//  Created by Polina Tikhomirova on 12.01.2023.
+//
+
+import UIKit
+
+enum AppStyles {
+    enum color {
+        static let sand = UIColor(hex: 0xDEAC8B),
+                   swamp = UIColor(hex: 0x3D4632),
+                   light = UIColor(hex: 0xF3EBE0),
+                   brown = UIColor(hex: 0x734E33),
+                   black = UIColor(hex: 0x000000),
+                   offwhite = UIColor(hex: 0xFFFFFF)
+    }
+}

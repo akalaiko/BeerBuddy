ca66f291c3fa8d46f1d3f1919f31fc147a7e3af8
Polina Tikhomirova <t_polinas@icloud.com>
2023-01-12 19:26:20 +0300

    Updated colors.

--- BeerBuddy/Core/Extensions/UIColor+Extensions.swift
+++ BeerBuddy/Core/Extensions/UIColor+Extensions.swift
@@ -7,38 +7,25 @@
 
 import UIKit
 
+struct AppColors {
+    static let sand = UIColor(hex: 0xDEAC8B)
+    static let swamp = UIColor(hex: 0x3D4632)
+    static let light = UIColor(hex: 0xF3EBE0)
+    static let brown = UIColor(hex: 0x734E33)
+    static let black = UIColor(hex: 0x000000)
+    static let offwhite = UIColor(hex: 0xFFFFFF)
+}
+
 extension UIColor {
-    static struct AppColors {
-        static let sand = UIColor(
-            red: 0.87,
-            green: 0.67,
-            blue: 0.55,
-            alpha: 1.00)
-        static let swamp = UIColor(
-            red: 0.24,
-            green: 0.27,
-            blue: 0.20,
-            alpha: 1.00)
-        static let light = UIColor(
-            red: 0.95,
-            green: 0.92,
-            blue: 0.88,
-            alpha: 1.00)
-        static let brown = UIColor(
-            red: 0.45,
-            green: 0.31,
-            blue: 0.20,
-            alpha: 1.00)
-        static let black = UIColor(
-            red: 0.00,
-            green: 0.00,
-            blue: 0.00,
-            alpha: 1.00)
-        static let offwhite = UIColor(
-            red: 1.00,
-            green: 1.00,
-            blue: 1.00,
-            alpha: 1.00)
+    convenience init(
+        hex: UInt,
+        alpha: CGFloat = 1
+    ) {
+        self.init(
+            red: CGFloat((hex & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((hex & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(hex & 0x0000FF) / 255.0,
+            alpha: alpha)
     }
 }
 
